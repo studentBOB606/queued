3425a8e534a6f17d2635cc3ebe44030bf4201dac
PandaDeveloper13 <9025264@student.zadkine.nl>
2026-06-16 11:38:10 +0200

    -Added list page -link links to correct pages

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/register', function () {
+    return view('signup');
+});
+Route::get('/films', function () {
+    return view('Film');
+});
+Route::get('/login', function () {
+    return view('Login');
+});
+Route::get('/list', function () {
+    return view('List');
+});
