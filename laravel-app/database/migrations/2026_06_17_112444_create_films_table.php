35e78b54ba85927e729ea872f06354b619ad9a9d
studentBOB606 <9025052@student.zadkine.nl>
2026-06-17 14:15:55 +0200

    film id
    (film_id) in the database gezet

--- laravel-app/database/migrations/2026_06_17_112444_create_films_table.php
+++ laravel-app/database/migrations/2026_06_17_112444_create_films_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
 Schema::create('films', function (Blueprint $table) {
-    $table->id();
+    $table->id('film_id');
     $table->string('title');
     $table->text('description');
     $table->year('release_year');
